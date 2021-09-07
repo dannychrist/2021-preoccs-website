0884c70c8086581d544c1ec229e3c450cb114f5a
Daniel Christiansen <dannyjameschrist@gmail.com>
2021-09-07 10:30:45 -0400

    src folder

---
+++ src/data/preoccupations.js
@@ -0,0 +1,296 @@
+// Covers
+import NewMaterial from '../assets/records/new-material.jpg';
+import SelfTitled from '../assets/records/self-titled.jpg';
+import VietCong from '../assets/records/viet-cong.jpg';
+import Cassette from '../assets/records/cassette.jpg';
+import SevenInch from '../assets/records/7Inch.jpg';
+import SplitSevenInch from '../assets/records/split7inch.jpg';
+
+export const Preoccupations = [
+  {
+    records: [
+      {
+        id: '001',
+        title: 'New Material',
+        label: 'Jagjaguwar/Flemish Eye (2018)',
+        cover: NewMaterial,
+        link: 'https://geni.us/preoccs_newmaterial',
+        songs: [
+          {
+            id: '001',
+            title: 'Espionage',
+            length: '4:29',
+          },
+          {
+            id: '002',
+            title: 'Decompose',
+            length: '4:19',
+          },
+          {
+            id: '003',
+            title: 'Disarray',
+            length: '3:49',
+            video: 'https://www.youtube.com/embed/keq2JOyZ6KI',
+          },
+          {
+            id: '004',
+            title: 'Manipulation',
+            length: '3:14',
+          },
+          {
+            id: '005',
+            title: 'Antidote',
+            length: '6:01',
+            video: 'https://www.youtube.com/embed/qRc9XW7ofGY',
+          },
+          {
+            id: '006',
+            title: 'Solace',
+            length: '4:32',
+          },
+          {
+            id: '007',
+            title: 'Doubt',
+            length: '4:40',
+          },
+          {
+            id: '008',
+            title: 'Compliance',
+            length: '5:08',
+          },
+        ],
+      },
+      {
+        id: '002',
+        title: 'Self-Titled',
+        label: 'Jagjaguwar/Flemish Eye (2016)',
+        cover: SelfTitled,
+        link: 'https://geni.us/preoccs_self-titled',
+        songs: [
+          {
+            id: '001',
+            title: 'Anxiety',
+            length: '4:29',
+            video: 'https://www.youtube.com/embed/csMbqrFT1dM',
+          },
+          {
+            id: '002',
+            title: 'Monotony',
+            length: '2:53',
+          },
+          {
+            id: '003',
+            title: 'Zodiac',
+            length: '3:43',
+          },
+          {
+            id: '004',
+            title: 'Memory',
+            length: '11:26',
+            video: 'https://www.youtube.com/embed/pQdVnKpv1Sk',
+          },
+          {
+            id: '005',
+            title: 'Degraded',
+            length: '4:00',
+            video: 'https://www.youtube.com/embed/vykhXVCSmz0',
+          },
+          {
+            id: '006',
+            title: 'Sense',
+            length: '1:01',
+          },
+          {
+            id: '007',
+            title: 'Forbidden',
+            length: '1:32',
+          },
+          {
+            id: '008',
+            title: 'Stimulation',
+            length: '4:41',
+          },
+          {
+            id: '009',
+            title: 'Fever',
+            length: '4:31',
+          },
+        ],
+      },
+      {
+        id: '003',
+        title: 'Viet-Cong',
+        label: 'Jagjaguwar/Flemish Eye (2015)',
+        cover: VietCong,
+        link: 'https://geni.us/preoccs_viet-cong',
+        songs: [
+          {
+            id: '001',
+            title: 'Newspaper Spoons',
+            length: '3:02',
+          },
+          {
+            id: '002',
+            title: 'Pointless Experience',
+            length: '2:59',
+          },
+          {
+            id: '003',
+            title: 'March of Progress',
+            length: '6:19',
+          },
+          {
+            id: '004',
+            title: 'Bunker Buster',
+            length: '5:55',
+            video: 'https://www.youtube.com/embed/ZOLIHJKCu8M',
+          },
+          {
+            id: '005',
+            title: 'Continental Shelf',
+            length: '3:20',
+            video: 'https://www.youtube.com/embed/hdMz7BUtOvk',
+          },
+          {
+            id: '006',
+            title: 'Silhouettes',
+            length: '4:12',
+            video: 'https://www.youtube.com/embed/zW1kP99mok4',
+          },
+          {
+            id: '007',
+            title: 'Death',
+            length: '11:17',
+          },
+        ],
+      },
+      {
+        id: '004',
+        title: 'Cassette',
+        label: 'Jagjaguwar/Flemish Eye (2014)',
+        cover: Cassette,
+        link: 'https://geni.us/preoccs_cassette',
+        songs: [
+          {
+            id: '001',
+            title: 'Throw It Away',
+            length: '3:09',
+          },
+          {
+            id: '002',
+            title: 'Unconscious Melody',
+            length: '3:00',
+          },
+          {
+            id: '003',
+            title: 'Oxygen Feed',
+            length: '3:50',
+          },
+          {
+            id: '004',
+            title: 'Static Wall',
+            length: '4:43',
+          },
+          {
+            id: '005',
+            title: 'Structureless Design',
+            length: '4:21',
+          },
+          {
+            id: '006',
+            title: 'Dark Entries (Bauhaus)',
+            length: '4:06',
+          },
+          {
+            id: '007',
+            title: 'Select Your Drone',
+            length: '7:15',
+          },
+        ],
+      },
+      {
+        id: '005',
+        title: '7 Inch',
+        label: 'Jagjaguwar/Flemish Eye (2016)',
+        link: 'https://geni.us/preoccs_7-inch',
+        cover:
+        SevenInch,
+        songs: [
+          {
+            id: '001',
+            title: 'Key (YMO)',
+            length: '4:37',
+          },
+          {
+            id: '002',
+            title: 'Off Duty Trip',
+            length: '3:36',
+          },
+        ],
+      },
+      {
+        id: '006',
+        title: 'Split 7 Inch w/ Protomartyr',
+        label: 'Domino Records (2018)',
+        cover: SplitSevenInch,
+        link: 'https://geni.us/preoccs_split-7-inch',
+        songs: [
+          {
+            id: '001',
+            title: 'Pontiac 87 (Protomartyr)',
+            length: '4:27',
+          },
+        ],
+      },
+    ],
+    videos: [
+      {
+        id: '001',
+        title: 'March of Progress, Canela Party',
+        link: 'https://www.youtube.com/embed/B2vaIO5WOsQ',
+      },
+      {
+        id: '003',
+        title: 'Dissary',
+        link: 'https://www.youtube.com/embed/keq2JOyZ6KI',
+      },
+      {
+        id: '004',
+        title: 'Antidote',
+        link: 'https://www.youtube.com/embed/qRc9XW7ofGY',
+      },
+      {
+        id: '005',
+        title: 'Anxiety',
+        link: 'https://www.youtube.com/embed/csMbqrFT1dM',
+      },
+      {
+        id: '006',
+        title: 'Memory',
+        link: 'https://www.youtube.com/embed/pQdVnKpv1Sk',
+      },
+      {
+        id: '007',
+        title: 'Degraded',
+        link: 'https://www.youtube.com/embed/vykhXVCSmz0',
+      },
+      {
+        id: '008',
+        title: 'Bunker Buster',
+        link: 'https://www.youtube.com/embed/ZOLIHJKCu8M',
+      },
+      {
+        id: '009',
+        title: 'Continental Shelf',
+        link: 'https://www.youtube.com/embed/hdMz7BUtOvk',
+      },
+      {
+        id: '010',
+        title: 'Silhouettes',
+        link: 'https://www.youtube.com/embed/zW1kP99mok4',
+      },
+    ],
+  },
+];
+
+export default Preoccupations;
---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavLinks.css';
+
+const NavLinks = (props) => {
+  return (
+    <ul className='nav-links'>
+      <li>
+        <NavLink to='/' exact>
+          HOME
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/music'>MUSIC</NavLink>
+      </li>
+      <li>
+        <NavLink to='/video'>VIDEO</NavLink>
+      </li>
+      <li>
+        <NavLink to='/live'>LIVE</NavLink>
+      </li>
+      <li>
+        <a href='https://www.hellomerch.com/collections/preoccupations'>SHOP</a>
+      </li>
+      <li>
+        <a href='https://preoccupations.us17.list-manage.com/subscribe?u=111ab42f11672c8db4328f3e7&id=6d6483a833'>
+          NEWSLETTER
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
