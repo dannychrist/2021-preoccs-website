df5122db28651881bd74655640bda3e345f34e63
Daniel Christiansen <danielchristiansen@Daniels-MacBook-Pro.local>
2025-02-03 21:23:21 -0500

    new changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM from 'react-dom'; // ✅ React 17 and earlier use 'react-dom'
 import './index.css';
 import App from './App';
 
+// ✅ Use ReactDOM.render() for React 17 or lower
 ReactDOM.render(
-<App />, document.getElementById('root'));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
