d6e04a97648a18d45180f86728b9fc1c4b438e42
Daniel Christiansen <danielchristiansen@Daniels-MacBook-Pro.local>
2025-02-09 12:20:38 -0500

    new changes

--- src/shared/components/UIElements/LoadingSpinner.js
+++ src/shared/components/UIElements/LoadingSpinner.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
 import './LoadingSpinner.css';
 
-const LoadingSpinner = (props) => {
+const LoadingSpinner = ({ asOverlay }) => {
   return (
-    <div className={`${props.asOverlay && 'loading-spinner__overlay'}`}>
-      <div className='lds-dual-ring'></div>
+    <div className={`${asOverlay ? 'loading-spinner__overlay' : ''}`}>
+      <div className='lds-ring'>
+        <div></div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
