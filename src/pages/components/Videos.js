4f9142c73f680606edabd5045f01c1d0f2e9f74f
Daniel Christiansen <dannyjameschrist@gmail.com>
2025-03-19 12:47:16 -0400

    new changes

--- src/pages/components/Videos.js
+++ src/pages/components/Videos.js
@@ -5,6 +5,7 @@ import Video from './Video';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 // Screenshotsill
+import Bastards from '../../assets/video-screenshots/bastards.webp';
 import Focus from '../../assets/video-screenshots/focus.webp';
 import Slowly from '../../assets/video-screenshots/slowly.webp';
 import DeathOfMelody from '../../assets/video-screenshots/deathofmelody.webp';
@@ -25,78 +26,84 @@ const Videos = () => {
   const musicvideos = [
     {
       id: '0000',
+      title: 'Bastards',
+      link: 'https://www.youtube-nocookie.com/embed/4RK_59EkZqE?start=2',
+      screenshot: Bastards,
+    },
+    {
+      id: '0001',
       title: 'Focus',
       link: 'https://www.youtube-nocookie.com/embed/npLTTvKpxPU?si=CANALUDNfDihzBRD',
       screenshot: Focus,
     },
     {
-      id: '0001',
+      id: '0002',
       title: 'Slowly',
       link: 'https://www.youtube-nocookie.com/embed/7NuwX0RZ6Hw',
       screenshot: Slowly,
     },
     {
-      id: '0002',
+      id: '0003',
       title: 'Death of Melody',
       link: 'https://www.youtube-nocookie.com/embed/yi6jdCx56AA',
       screenshot: DeathOfMelody,
     },
     {
-      id: '0003',
+      id: '0004',
       title: 'Ricochet',
       link: 'https://www.youtube-nocookie.com/embed/v1hJz7xjFTY',
       screenshot: Ricochet,
     },
     {
-      id: '0004',
+      id: '0005',
       title: 'March of Progress, Canela Party',
       link: 'https://www.youtube-nocookie.com/embed/B2vaIO5WOsQ',
       screenshot: CanelaParty,
     },
     {
-      id: '0005',
+      id: '0006',
       title: 'Disarray',
       link: 'https://www.youtube-nocookie.com/embed/keq2JOyZ6KI',
       screenshot: Disarray,
     },
     {
-      id: '0006',
+      id: '0007',
       title: 'Antidote',
       link: 'https://www.youtube-nocookie.com/embed/qRc9XW7ofGY',
       screenshot: Antidote,
     },
     {
-      id: '0007',
+      id: '0008',
       title: 'Anxiety',
       link: 'https://www.youtube-nocookie.com/embed/csMbqrFT1dM',
       screenshot: Anxiety,
     },
     {
-      id: '0008',
+      id: '0009',
       title: 'Memory',
       link: 'https://www.youtube-nocookie.com/embed/pQdVnKpv1Sk',
       screenshot: Memory,
     },
     {
-      id: '0009',
+      id: '0010',
       title: 'Degraded',
       link: 'https://www.youtube-nocookie.com/embed/vykhXVCSmz0',
       screenshot: Degraded,
     },
     {
-      id: '0010',
+      id: '0011',
       title: 'Bunker Buster',
       link: 'https://www.youtube-nocookie.com/embed/ZOLIHJKCu8M',
       screenshot: BunkerBuster,
     },
     {
-      id: '0011',
+      id: '0012',
       title: 'Continental Shelf',
       link: 'https://www.youtube-nocookie.com/embed/hdMz7BUtOvk',
       screenshot: ContinentalShelf,
     },
     {
-      id: '0012',
+      id: '0013',
       title: 'Silhouettes',
       link: 'https://www.youtube-nocookie.com/embed/zW1kP99mok4',
       screenshot: Silhouettes,
